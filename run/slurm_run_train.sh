7395806ef33f41e40f6893700ad1a9f8f8e1504d
Yu Wang <wangyu@in.tum.de>
2019-02-18 12:53:45 +0100

    update slurm run bash script to use more memory

--- run/slurm_run_train.sh
+++ run/slurm_run_train.sh
@@ -2,12 +2,11 @@
 #SBATCH --job-name=train_gpu4_b256_sgd
 #SBATCH --nodes=1
 #SBATCH --cpus=16
-#SBATCH --mem=64GB
+#SBATCH --mem=72GB
 #SBATCH --gres=gpu:titanxpascal:4
 #SBATCH --gres-flags=enforce-binding
 #SBATCH --time=7-00:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_50, TIME_LIMIT_90
-#SBATCH --output=/usr/stud/wangyu/PycharmProjects/slurm_log/slurm-%j.out
 
 export PYTHONHOME=/usr/stud/wangyu/venv
 srun /usr/stud/wangyu/venv/bin/python train_imagenet.py
